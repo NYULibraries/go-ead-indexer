f18875947c6fd1f1254ccef00d07734ff4c233b3
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2024-09-18 18:44:20 -0400

    Add language_test.go with helper functions and test cases for language code errors

---
+++ pkg/language/language_test.go
@@ -0,0 +1,43 @@
+package language
+
+import (
+	"testing"
+)
+
+func assertError(t *testing.T, err error, expectedErr error, testCase string) {
+	if err == nil || err.Error() != expectedErr.Error() {
+		t.Errorf("Expected error: %s, got: %s for test case: %s", expectedErr, err, testCase)
+	}
+}
+
+func assertLanguage(t *testing.T, language string, expectedLanguage string, testCase string) {
+	if language != expectedLanguage {
+		t.Errorf("Expected language: %s, got: %s for test case: %s ", expectedLanguage, language, testCase)
+	}
+}
+
+func TestGetLanguageForLanguageCode_Errors(t *testing.T) {
+	_, err := GetLanguageForLanguageCode("abcd")
+	assertError(t, err, ErrInvalidLength, "invalid length")
+
+	_, err = GetLanguageForLanguageCode("")
+	assertError(t, err, ErrEmptyLanguageCode, "empty string")
+
+	_, err = GetLanguageForLanguageCode("a a")
+	assertError(t, err, ErrInternalWhitespace, "internal whitespace")
+
+	_, err = GetLanguageForLanguageCode(" aR")
+	assertError(t, err, ErrLanguageNotFound, "invalid language code with leading space")
+
+	_, err = GetLanguageForLanguageCode("Ra ")
+	assertError(t, err, ErrLanguageNotFound, "invalid language code with trailing space")
+
+	_, err = GetLanguageForLanguageCode("zpy")
+	assertError(t, err, ErrLanguageNotFound, "non-existing language code")
+
+	_, err = GetLanguageForLanguageCode("en1")
+	assertError(t, err, ErrInvalidCharacters, "language code contains invalid characters")
+
+	_, err = GetLanguageForLanguageCode("!es#")
+	assertError(t, err, ErrInvalidCharacters, "language code contains invalid characters")
+}
