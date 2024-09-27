9083b4b9dd86ba4afe6d9ce387ee491d403e0f5d
David <da70@nyu.edu>
2024-09-27 18:29:01 -0400

    language_test.go: order functions with tests at the top and non-exported helpers at the bottom, and alphasort

--- pkg/language/language_test.go
+++ pkg/language/language_test.go
@@ -6,34 +6,33 @@ import (
 	"testing"
 )
 
-func assertError(t *testing.T, err error, expectedErr error, testCase string) {
-	if err == nil || err.Error() != expectedErr.Error() {
-		t.Errorf("Expected error for test case \"%s\": \"%s\", got: \"%s\"",
-			testCase, expectedErr, err)
+func TestGetLanguageForLanguageCode(t *testing.T) {
+	var tests = []struct {
+		name          string
+		inputModifier func(string) string
+	}{
+		{"lowercase", func(code string) string { return strings.ToLower(code) }},
+		{"uppercase", func(code string) string { return strings.ToUpper(code) }},
+		{"mixed-case", func(code string) string {
+			return strings.ToLower(code[:1]) + strings.ToUpper(code[1:])
+		}},
+		{"lowercase with trimmable whitespace", func(code string) string { return " " + code + " " }},
+		{"valid code with trimmable newline", func(code string) string { return code + "\n" }},
+		{"valid code with trimmable carriage return", func(code string) string { return code + "\r" }},
+		{"valid code with trimmable newline and carriage return", func(code string) string { return code + "\n\r" }},
+		{"valid code with trimmable tab", func(code string) string { return code + "\t" }},
 	}
-}
 
-func assertLanguage(t *testing.T, language string, expectedLanguage string, err error, testCase string) {
-	if err != nil {
-		t.Errorf("Unexpected error for test case \"%s\": \"%v\" ", testCase, err)
-	} else if language != expectedLanguage {
-		t.Errorf("Expected language for test case \"%s\": \"%s\", got: \"%s\"",
-			testCase, expectedLanguage, language)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			for code, expectedLanguage := range ExpectedTestLanguages {
+				modifiedCode := test.inputModifier(code)
+				result, err := GetLanguageForLanguageCode(modifiedCode)
+				assertLanguage(t, result, expectedLanguage, err, test.name+
+					" for '"+escapeForReadability(modifiedCode)+"'")
+			}
+		})
 	}
-}
-
-// Convert newlines, carriage returns, and tabs to escapes "\n", "\r", and "\t" for
-// safe and more readable printing in test results and logs.
-func escapeForReadability(code string) string {
-	carriageReturnRegexp := regexp.MustCompile(`\r`)
-	newlineRegexp := regexp.MustCompile(`\n`)
-	tabRegexp := regexp.MustCompile(`\t`)
-
-	code = carriageReturnRegexp.ReplaceAllString(code, "\\r")
-	code = newlineRegexp.ReplaceAllString(code, "\\n")
-	code = tabRegexp.ReplaceAllString(code, "\\t")
-
-	return code
 }
 
 func TestGetLanguageForLanguageCode_InvalidLanguageCodeCases(t *testing.T) {
@@ -59,6 +58,7 @@ func TestGetLanguageForLanguageCode_InvalidLanguageCodeCases(t *testing.T) {
 	}
 
 }
+
 func TestGetLanguageForLanguageCode_UntrimmableWhitespaceErrors(t *testing.T) {
 	var tests = []struct {
 		name          string
@@ -87,31 +87,32 @@ func TestGetLanguageForLanguageCode_UntrimmableWhitespaceErrors(t *testing.T) {
 	}
 }
 
-func TestGetLanguageForLanguageCode(t *testing.T) {
-	var tests = []struct {
-		name          string
-		inputModifier func(string) string
-	}{
-		{"lowercase", func(code string) string { return strings.ToLower(code) }},
-		{"uppercase", func(code string) string { return strings.ToUpper(code) }},
-		{"mixed-case", func(code string) string {
-			return strings.ToLower(code[:1]) + strings.ToUpper(code[1:])
-		}},
-		{"lowercase with trimmable whitespace", func(code string) string { return " " + code + " " }},
-		{"valid code with trimmable newline", func(code string) string { return code + "\n" }},
-		{"valid code with trimmable carriage return", func(code string) string { return code + "\r" }},
-		{"valid code with trimmable newline and carriage return", func(code string) string { return code + "\n\r" }},
-		{"valid code with trimmable tab", func(code string) string { return code + "\t" }},
+func assertError(t *testing.T, err error, expectedErr error, testCase string) {
+	if err == nil || err.Error() != expectedErr.Error() {
+		t.Errorf("Expected error for test case \"%s\": \"%s\", got: \"%s\"",
+			testCase, expectedErr, err)
 	}
+}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			for code, expectedLanguage := range ExpectedTestLanguages {
-				modifiedCode := test.inputModifier(code)
-				result, err := GetLanguageForLanguageCode(modifiedCode)
-				assertLanguage(t, result, expectedLanguage, err, test.name+
-					" for '"+escapeForReadability(modifiedCode)+"'")
-			}
-		})
+func assertLanguage(t *testing.T, language string, expectedLanguage string, err error, testCase string) {
+	if err != nil {
+		t.Errorf("Unexpected error for test case \"%s\": \"%v\" ", testCase, err)
+	} else if language != expectedLanguage {
+		t.Errorf("Expected language for test case \"%s\": \"%s\", got: \"%s\"",
+			testCase, expectedLanguage, language)
 	}
+}
+
+// Convert newlines, carriage returns, and tabs to escapes "\n", "\r", and "\t" for
+// safe and more readable printing in test results and logs.
+func escapeForReadability(code string) string {
+	carriageReturnRegexp := regexp.MustCompile(`\r`)
+	newlineRegexp := regexp.MustCompile(`\n`)
+	tabRegexp := regexp.MustCompile(`\t`)
+
+	code = carriageReturnRegexp.ReplaceAllString(code, "\\r")
+	code = newlineRegexp.ReplaceAllString(code, "\\n")
+	code = tabRegexp.ReplaceAllString(code, "\\t")
+
+	return code
 }
