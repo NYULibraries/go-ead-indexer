77360ca8691b51f5b95af674a71b7ca460492a56
David <da70@nyu.edu>
2024-09-27 17:03:14 -0400

    pkg/language/language.go: reword a few error messages; extract MIN/MAX langcode length consts; add a comment explaining why we can't refer user to the official ISO-639-2 list

--- pkg/language/language.go
+++ pkg/language/language.go
@@ -2,16 +2,27 @@ package language
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 	"unicode"
 )
 
+const MIN_LANGUAGE_CODE_LENGTH = 2
+const MAX_LANGUAGE_CODE_LENGTH = 3
+
 var (
-	ErrEmptyLanguageCode  = errors.New("language code cannot be empty")
-	ErrInvalidCharacters  = errors.New("invalid characters provided")
-	ErrInvalidLength      = errors.New("language code must be either 2 or 3 characters long")
-	ErrInternalWhitespace = errors.New("language code contains invalid whitespace characters")
-	ErrLanguageNotFound   = errors.New("language code not found. Please refer to ISO-639-2 language code table")
+	ErrEmptyLanguageCode = errors.New("language code cannot be empty")
+	ErrInvalidCharacters = errors.New("language code contains invalid characters")
+	ErrInvalidLength     = errors.New(
+		fmt.Sprintf("language code must be between %d-%d characters long, inclusive",
+			MIN_LANGUAGE_CODE_LENGTH, MAX_LANGUAGE_CODE_LENGTH))
+	ErrInternalWhitespace = errors.New("language code contains internal whitespace")
+	// We can't refer the user to the official ISO-639-2 language code list because
+	// the list changes over time and is not versioned.  We are in fact currently
+	// using an out of date ISO-639-2 language code map in order to exactly match
+	// the v1 indexer translations.  For details, see this comment in DLFA-224:
+	// https://jira.nyu.edu/browse/DLFA-224?focusedCommentId=9870333&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-9870333
+	ErrLanguageNotFound = errors.New("language code not found.  Please refer to the source code of this package for accepted language codes.")
 )
 
 func GetLanguageForLanguageCode(languageCode string) (string, error) {
@@ -29,7 +40,8 @@ func GetLanguageForLanguageCode(languageCode string) (string, error) {
 		return "", ErrInvalidCharacters
 	}
 
-	if len(languageCode) < 2 || len(languageCode) > 3 {
+	if len(languageCode) < MIN_LANGUAGE_CODE_LENGTH ||
+		len(languageCode) > MAX_LANGUAGE_CODE_LENGTH {
 		return "", ErrInvalidLength
 	}
 
