047269b7716f2dfb591761e1d52a50fa8c06241e
David <da70@nyu.edu>
2024-10-11 17:37:30 -0400

    pkg/ead/: set up basic EAD parsing with lestrrat-go/libxml2, and explain why we have to blank out the `xlmns` attribute using string replace

--- go.mod
+++ go.mod
@@ -2,9 +2,18 @@ module go-ead-indexer
 
 go 1.23.1
 
-require github.com/spf13/cobra v1.8.1
+require (
+	github.com/antchfx/xmlquery v1.4.2
+	github.com/lestrrat-go/libxml2 v0.0.0-20240905100032-c934e3fcb9d3
+	github.com/spf13/cobra v1.8.1
+)
 
 require (
+	github.com/antchfx/xpath v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
--- pkg/ead/ead.go
+++ pkg/ead/ead.go
@@ -1,16 +1,93 @@
 package ead
 
+import (
+	"fmt"
+	"github.com/lestrrat-go/libxml2/parser"
+	"github.com/lestrrat-go/libxml2/types"
+	"github.com/lestrrat-go/libxml2/xpath"
+	"regexp"
+)
+
+// We need to set `xmlns=""` to get the xpath queries working.  See code comment
+// in `New()` for more details.  `xmlns=""` is valid according to this post:
+// https://stackoverflow.com/questions/1587891/is-xmlns-a-valid-xml-namespace
+var namespaceRegexp = regexp.MustCompile(`<((?s)\s*)ead((?s).*)xmlns="(?U).*"`)
+
 type EAD struct {
-	Collection Collection
-	Components *[]Component
+	Collection           Collection
+	Components           *[]Component
+	ModifiedFileContents string
+	OriginalFileContents string
 }
 
 func New(eadXML string) (EAD, error) {
 	ead := EAD{}
 
-	// TODO: Remove this fake data
-	ead.Collection.SolrAddMessage = ""
-	ead.Components = &[]Component{
+	// XPath queries fail if we don't set the namespace to empty string.
+	// Excepting the `xlink` prefix, the tags in the EAD files don't seem to use
+	// namespace prefixes much, and the XPath queries we need for this indexer
+	// don't use prefixes at all.  Some brief experimentation suggests that if
+	// we don't blank out the `xmlns` attribute, we would have to register a
+	// namespace and add the prefix to all tag names in all the xpath queries.
+	// We do a string replace instead of using `ctx.RegisterNS("", "")`, because
+	// that call fails with the error "cannot register namespace".
+	//
+	// Note: v1 indexer removed all namespace stuff including prefixes:
+	// https://github.com/awead/solr_ead/blob/v0.7.5/lib/solr_ead/om_behaviors.rb#L24
+	// There doesn't appear to be a way to do this using `lestrrat-go/libxml2`.
+	// There is one method `Element.SetNamespace()` which appears to enable
+	// setting the namespace URL and prefix to empty strings, but the methods we
+	// use for parsing and traversing/querying the DOM all seem to return `Node`
+	// objects, not `Element` objects, and `Node` does not have a `SetNamespace()`
+	// method.
+	// A quick online search didn't turn up any easy to implement solutions for
+	// removing namespace stuff from all nodes using the standard library
+	// `encoding/xml` package.
+	matchGroups := namespaceRegexp.FindStringSubmatch(eadXML)
+	newString := fmt.Sprintf(`<%sead%sxmlns=""`, matchGroups[1], matchGroups[2])
+	modifiedEADXML := namespaceRegexp.ReplaceAllString(eadXML, newString)
+
+	ead.OriginalFileContents = eadXML
+	ead.ModifiedFileContents = modifiedEADXML
+
+	xmlDoc, err := MakeXMLDoc(modifiedEADXML)
+	defer xmlDoc.Free()
+	if err != nil {
+		return ead, err
+	}
+
+	rootNode, err := xmlDoc.DocumentElement()
+	if err != nil {
+		return ead, err
+	}
+
+	ead.Collection, err = MakeCollection(rootNode)
+	if err != nil {
+		return ead, err
+	}
+
+	ead.Components, err = MakeComponents(rootNode)
+	if err != nil {
+		return ead, err
+	}
+
+	// Test
+	unittitles := xpath.String(rootNode.Find("//archdesc[@level='collection']/did/unittitle"))
+	fmt.Println(unittitles)
+
+	return ead, nil
+}
+
+func MakeCollection(node types.Node) (Collection, error) {
+	// TODO: remove this fake data
+	return Collection{
+		SolrAddMessage: "",
+	}, nil
+}
+
+func MakeComponents(node types.Node) (*[]Component, error) {
+	// TODO: remove this fake data
+	return &[]Component{
 		{
 			ID:             "mos_2021additional-daos",
 			SolrAddMessage: "",
@@ -23,7 +100,15 @@ func New(eadXML string) (EAD, error) {
 			ID:             "mos_2021dao1",
 			SolrAddMessage: "",
 		},
+	}, nil
+}
+
+func MakeXMLDoc(eadXML string) (types.Document, error) {
+	xmlParser := parser.New()
+	xmlDoc, err := xmlParser.ParseString(eadXML)
+	if err != nil {
+		return xmlDoc, err
 	}
 
-	return ead, nil
+	return xmlDoc, nil
 }
