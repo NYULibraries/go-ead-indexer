d9ed08c007b2c3c763710b5a291ed4ff5b04423e
David <da70@nyu.edu>
2024-11-05 10:01:05 -0500

    ead/collectiondoc/solr: alphasort `DocElement` fields

--- pkg/ead/collectiondoc/solr.go
+++ pkg/ead/collectiondoc/solr.go
@@ -9,8 +9,8 @@ type AddElement struct {
 }
 
 type DocElement struct {
-	ID             string `xml:"id"`
 	EAD_SSI        string `xml:"ead_ssi"`
+	ID             string `xml:"id"`
 	Repository_SIM string `xml:"repository_sim"`
 	Repository_SSI string `xml:"repository_ssi"`
 	Repository_SSM string `xml:"repository_ssm"`
@@ -19,8 +19,8 @@ type DocElement struct {
 func (collectionDoc *CollectionDoc) setSolrAddMessage() {
 	docElement := &collectionDoc.SolrAddMessage.Add.Doc
 
-	docElement.ID = collectionDoc.Parts.EADID.Values[0]
 	docElement.EAD_SSI = collectionDoc.Parts.EADID.Values[0]
+	docElement.ID = collectionDoc.Parts.EADID.Values[0]
 	docElement.Repository_SIM = collectionDoc.Parts.RepositoryCode.Values[0]
 	docElement.Repository_SSI = collectionDoc.Parts.RepositoryCode.Values[0]
 	docElement.Repository_SSM = collectionDoc.Parts.RepositoryCode.Values[0]
