4e017bd23bbc97f2c754c4973d77d9b983a02c77
David <da70@nyu.edu>
2024-11-05 10:15:11 -0500

    pkg/ead/collectiondoc/solr.go: lowercase the Solr field suffixes in field names

--- pkg/ead/collectiondoc/solr.go
+++ pkg/ead/collectiondoc/solr.go
@@ -9,21 +9,21 @@ type AddElement struct {
 }
 
 type DocElement struct {
-	EAD_SSI        string `xml:"ead_ssi"`
-	ID             string `xml:"id"`
-	Repository_SIM string `xml:"repository_sim"`
-	Repository_SSI string `xml:"repository_ssi"`
-	Repository_SSM string `xml:"repository_ssm"`
+	EAD_ssi        string   `xml:"ead_ssi"`
+	ID             string   `xml:"id"`
+	Repository_sim string   `xml:"repository_sim"`
+	Repository_ssi string   `xml:"repository_ssi"`
+	Repository_ssm string   `xml:"repository_ssm"`
 }
 
 func (collectionDoc *CollectionDoc) setSolrAddMessage() {
 	docElement := &collectionDoc.SolrAddMessage.Add.Doc
 
-	docElement.EAD_SSI = collectionDoc.Parts.EADID.Values[0]
+	docElement.EAD_ssi = collectionDoc.Parts.EADID.Values[0]
 	docElement.ID = collectionDoc.Parts.EADID.Values[0]
-	docElement.Repository_SIM = collectionDoc.Parts.RepositoryCode.Values[0]
-	docElement.Repository_SSI = collectionDoc.Parts.RepositoryCode.Values[0]
-	docElement.Repository_SSM = collectionDoc.Parts.RepositoryCode.Values[0]
+	docElement.Repository_sim = collectionDoc.Parts.RepositoryCode.Values[0]
+	docElement.Repository_ssi = collectionDoc.Parts.RepositoryCode.Values[0]
+	docElement.Repository_ssm = collectionDoc.Parts.RepositoryCode.Values[0]
 }
 
 func (solrAddMessage *SolrAddMessage) String() string {
