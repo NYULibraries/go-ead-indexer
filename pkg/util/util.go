393122edc42dcbf4d000abf3f7cf2adb841bb28e
David <da70@nyu.edu>
2024-11-11 11:40:15 -0500

    pkg/util/util.go: alpha-sort function definitions

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -5,6 +5,23 @@ import (
 	"os"
 )
 
+// Replicate https://github.com/NYULibraries/ead_indexer/blob/a367ab8cc791376f0d8a287cbcd5b6ee43d5c04f/lib/ead_indexer/behaviors.rb#L137
+// TODO: If it turns out that we don't need to preserve order in any of our uniq'ed
+// slices, remove this.
+func CompactStringSlicePreserveOrder(stringSlice []string) []string {
+	compactedSlice := []string{}
+	seen := map[string]bool{}
+	for _, element := range stringSlice {
+		if seen[element] {
+			continue
+		}
+		compactedSlice = append(compactedSlice, element)
+		seen[element] = true
+	}
+
+	return compactedSlice
+}
+
 func DiffFiles(path1 string, path2 string) (string, error) {
 	bytes1, err := os.ReadFile(path1)
 	if err != nil {
@@ -25,21 +42,4 @@ func DiffStrings(label1 string, string1 string, label2, string2 string) string {
 	diffString := string(diff.Diff(label1, []byte(string1), label2, []byte(string2)))
 
 	return diffString
-}
-
-// Replicate https://github.com/NYULibraries/ead_indexer/blob/a367ab8cc791376f0d8a287cbcd5b6ee43d5c04f/lib/ead_indexer/behaviors.rb#L137
-// TODO: If it turns out that we don't need to preserve order in any of our uniq'ed
-// slices, remove this.
-func CompactStringSlicePreserveOrder(stringSlice []string) []string {
-	compactedSlice := []string{}
-	seen := map[string]bool{}
-	for _, element := range stringSlice {
-		if seen[element] {
-			continue
-		}
-		compactedSlice = append(compactedSlice, element)
-		seen[element] = true
-	}
-
-	return compactedSlice
 }
