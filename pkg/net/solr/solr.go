1e5dfc5eb2526a292e3b87709971ba3eb5f85bc9
David <da70@nyu.edu>
2024-12-12 11:39:48 -0500

    pkg/net/solr/solr.go: add fake Add() stuff so can complete TestAdd()

--- pkg/net/solr/solr.go
+++ pkg/net/solr/solr.go
@@ -14,7 +14,22 @@ import (
 // accidental misconfiguration of an instance.
 var solrURLOrigin string
 
+// TODO: Obviously replace this fake stuff after `TestAdd()` is completed.  There
+// needs to be actual files written out for the test to do diff'ing against.
 func Add(xmlPostBody string) error {
+	var idRegExp = regexp.MustCompile(`<field name="id">([a-z0-9_-]+)</field>`)
+
+	matches := idRegExp.FindStringSubmatch(xmlPostBody)
+	if len(matches) < 2 {
+		return errors.New("No id found")
+	}
+
+	id := matches[1]
+
+	postBody := []byte(`<field name="id">` + id + "</field>")
+	responseBody := bytes.NewBuffer(postBody)
+	_, _ = http.Post(solrURLOrigin, "text/xml", responseBody)
+
 	return nil
 }
 
