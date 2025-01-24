b2ebfdabbd22b1dce8e586044409d3f28e33bf3d
jgpawletko <jgpawletko@gmail.com>
2025-01-24 17:18:39 -0500

    update gen-repo.sh
    Update comments.
    Change single quotes to double quotes in commit messages.

--- pkg/git/testsupport/gen-repo.bash
+++ pkg/git/testsupport/gen-repo.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # This script generates a git repo test fixture for use in the git package tests.
-# The script creates a directory named 'simple-repo', then generates and commits various files
-# finally, the script creates a tarball named 'simple-repo.tar.gz'
-# This file can be copied into the pkg/git/testdata directory for use in git pkg tests
+# The script creates a directory named 'simple-repo', then generates and commits various files.
+# Finally, the script creates a tarball named 'simple-repo.tar.gz'.
+# The tarball can be copied into the pkg/git/testdata directory for use in git pkg tests.
 
 # Commit history replicated in repo (NOTE: commit hashes may differ)
-# * ba7d9b00023a8d6ed962e46465d800265a6d06b9 2025-01-03 17:05:48 -0500 | Updating file archives/mc_1.xml, Deleting file fales/mss_002.xml EADID='mss_002', Updating file fales/mss_005.xml, Updating file tamwag/aia_002.xml (HEAD -> main) [jgpawletko]
-# * ca2dd426b9f22b52e101e71ce8db83c80508df06 2025-01-03 17:00:32 -0500 | Updating file archives/cap_1.xml, Updating file fales/mss_004.xml, Updating file tamwag/aia_001.xml [jgpawletko]
-# * ae25d50165da2befdfc21624ba52241ad36070de 2025-01-03 16:55:07 -0500 | Updating file fales/mss_002.xml, Updating file fales/mss_003.xml [jgpawletko]
-# * d9fa76ef7c89994d8d3ed458e5c06b2c5bb9f414 2025-01-03 16:54:16 -0500 | Updating file fales/mss_001.xml [jgpawletko]
+# * 95f2f904ad261e7d31632021fa10768d2b4096c9 2025-01-24 17:10:44 -0500 | Updating file fales/mss_001.xml (HEAD -> main) [jgpawletko]
+# * aa58b2314e11ae5af61129ebfe1ceb07b49c2d33 2025-01-24 17:10:44 -0500 | Updating file archives/mc_1.xml, Deleting file fales/mss_002.xml EADID='mss_002', Updating file fales/mss_005.xml, Updating file tamwag/aia_002.xml [jgpawletko]
+# * 3dc6fabe0fcd990e95cdd3f88cff821196fccdbd 2025-01-24 17:10:44 -0500 | Updating file archives/cap_1.xml, Updating file fales/mss_004.xml, Updating file tamwag/aia_001.xml [jgpawletko]
+# * 7fe6de7c56d30149889f8d24eaf2fa66ed9f2e2d 2025-01-24 17:10:44 -0500 | Updating file fales/mss_002.xml, Updating file fales/mss_003.xml [jgpawletko]
+# * 155309f674b5acffd7473c1648f3647a2a3d242b 2025-01-24 17:10:44 -0500 | Updating file fales/mss_001.xml [jgpawletko]
 
 err_exit() {
     echo "$@" 1>&2
@@ -39,19 +40,19 @@ pushd simple-repo || err_exit "Failed to change directory to simple-repo"
 git init .
 
 git add fales/mss_001.xml
-git commit -m 'Updating file fales/mss_001.xml'
+git commit -m "Updating file fales/mss_001.xml"
 
 git add fales/mss_002.xml fales/mss_003.xml
-git commit -m 'Updating file fales/mss_002.xml, Updating file fales/mss_003.xml'
+git commit -m "Updating file fales/mss_002.xml, Updating file fales/mss_003.xml"
 
 git add archives/cap_1.xml fales/mss_004.xml tamwag/aia_001.xml
-git commit -m 'Updating file archives/cap_1.xml, Updating file fales/mss_004.xml, Updating file tamwag/aia_001.xml'
+git commit -m "Updating file archives/cap_1.xml, Updating file fales/mss_004.xml, Updating file tamwag/aia_001.xml"
 
 git add archives/mc_1.xml
 git rm fales/mss_002.xml
 git add fales/mss_005.xml
 git add tamwag/aia_002.xml
-git commit -m 'Updating file archives/mc_1.xml, Deleting file fales/mss_002.xml EADID='mss_002', Updating file fales/mss_005.xml, Updating file tamwag/aia_002.xml'
+git commit -m "Updating file archives/mc_1.xml, Deleting file fales/mss_002.xml EADID='mss_002', Updating file fales/mss_005.xml, Updating file tamwag/aia_002.xml"
 
 echo "mss_001 update" > fales/mss_001.xml
 git add fales/mss_001.xml
