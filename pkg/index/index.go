c7c078c1df325a476f84eb0548ec135406aec793
Eric Griffis <eric.griffis@nyu.edu>
2025-02-21 16:06:52 -0500

    Try to create minimal example of static linking issue

--- pkg/index/index.go
+++ pkg/index/index.go
@@ -1,3 +1,14 @@
 package index
 
+import (
+	"github.com/nyulibraries/go-ead-indexer/pkg/ead"
+)
+
 const MessageKey = "message"
+
+func hello() {
+	_, err := ead.New("hello", "world")
+	if err != nil {
+		panic(err)
+	}
+}
