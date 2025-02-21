5941d449844478e8883ea4fd5eb2bfc79a5497f9
jgpawletko <jgpawletko@gmail.com>
2025-02-21 08:53:57 -0500

    update index package
    Add a call to SolrClient.Delete() to the beginning of index.IndexEADFile().
    
    Update test mock to record the EADID passed to the SolrClient.Delete()
      function as well as the order in which the SolrClient.Delete() and
      SolrClient.Commit() functions were called.
    
    Perform housekeeping on the import statements.

--- pkg/index/index_test.go
+++ pkg/index/index_test.go
@@ -1,14 +1,17 @@
 package index
 
 import (
-	"go-ead-indexer/pkg/index/testutils"
 	"path/filepath"
 	"testing"
+
+	"github.com/nyulibraries/go-ead-indexer/pkg/index/testutils"
 )
 
 func TestAdd(t *testing.T) {
 
 	var testFixturePath string
+	partner := "fales"
+	eadid := "mss_460"
 
 	pwd, err := filepath.Abs(".")
 	if err != nil {
@@ -20,8 +23,8 @@ func TestAdd(t *testing.T) {
 	// need to get to: /root/path/to/go-ead-indexer/pkg/ead/testdata/fixtures/
 	testFixturePath = filepath.Join(pwd, "..", "ead", "testdata")
 
-	var eadPath = filepath.Join(testFixturePath, "fixtures", "ead-files", "fales", "mss_460.xml")
-	var xmlDir = filepath.Join(testFixturePath, "golden", "fales", "mss_460")
+	var eadPath = filepath.Join(testFixturePath, "fixtures", "ead-files", partner, eadid+".xml")
+	var xmlDir = filepath.Join(testFixturePath, "golden", partner, eadid)
 
 	sc := testutils.GetSolrClientMock()
 	err = sc.SetupMock(xmlDir)
@@ -42,5 +45,20 @@ func TestAdd(t *testing.T) {
 	// Check if the operation is complete from the Solr client perspective
 	if !sc.IsComplete() {
 		t.Errorf("Not all files were added to the Solr index. Remaining values: %v", sc.GoldenFileHashes)
+	}
+
+	// check that delete was called first
+	if sc.DeleteCallOrder != 1 {
+		t.Errorf("Delete was not called first. Call order: %d", sc.DeleteCallOrder)
+	}
+	if sc.DeleteArgument != eadid {
+		t.Errorf("Delete was not called with the correct argument. expected: %s, got: %s", eadid, sc.DeleteArgument)
+	}
+
+	// check that commit was called in the expected sequence
+	// the mock increments the call count before storing the value
+	// so: delete + number of files + commit = number of files + 2
+	if sc.CommitCallOrder != sc.NumberOfFilesToIndex+2 {
+		t.Errorf("Commit was not called at the expected time. Expected: %d, got: %d", sc.NumberOfFilesToIndex+1, sc.CommitCallOrder)
 	}
 }
