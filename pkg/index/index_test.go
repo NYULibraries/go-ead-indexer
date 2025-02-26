2fa841c9745ab302f27fc5c4a2f7d23a62502f69
jgpawletko <jgpawletko@gmail.com>
2025-02-26 12:44:33 -0500

    WIP: snapshot index with Rollback() calls
    Snapshot initial index with Rollback + Mock with error return capability

--- pkg/index/index_test.go
+++ pkg/index/index_test.go
@@ -27,11 +27,72 @@ func TestAdd(t *testing.T) {
 	var xmlDir = filepath.Join(testFixturePath, "golden", partner, eadid)
 
 	sc := testutils.GetSolrClientMock()
-	err = sc.SetupMock(xmlDir)
+	err = sc.InitMock(xmlDir)
 	if err != nil {
 		t.Errorf("Error setting Solr client: %s", err)
 		t.FailNow()
 	}
+
+	// Set the Solr client
+	SetSolrClient(sc)
+
+	// Index the EAD file
+	errs := IndexEADFile(eadPath)
+	if len(errs) > 0 {
+		t.Errorf("Error indexing EAD file: %s", errs)
+	}
+
+	// Check if the operation is complete from the Solr client perspective
+	if !sc.IsComplete() {
+		t.Errorf("Not all files were added to the Solr index. Remaining values: %v", sc.GoldenFileHashes)
+	}
+
+	// check that delete was called first
+	if sc.DeleteCallOrder != 1 {
+		t.Errorf("Delete was not called first. Call order: %d", sc.DeleteCallOrder)
+	}
+	if sc.DeleteArgument != eadid {
+		t.Errorf("Delete was not called with the correct argument. expected: %s, got: %s", eadid, sc.DeleteArgument)
+	}
+
+	// check that commit was called in the expected sequence
+	// the mock increments the call count before storing the value
+	// so: delete + number of files + commit = number of files + 2
+	if sc.CommitCallOrder != sc.NumberOfFilesToIndex+2 {
+		t.Errorf("Commit was not called at the expected time. Expected: %d, got: %d", sc.NumberOfFilesToIndex+1, sc.CommitCallOrder)
+	}
+}
+
+func TestRollback(t *testing.T) {
+
+	var testFixturePath string
+	partner := "nyhs"
+	eadid := "ms347_foundling_hospital"
+
+	pwd, err := filepath.Abs(".")
+	if err != nil {
+		t.Errorf("Error getting absolute path for pwd: %s", err)
+		t.FailNow()
+	}
+
+	// pwd should be /root/path/to/go-ead-indexer/pkg/index/
+	// need to get to: /root/path/to/go-ead-indexer/pkg/ead/testdata/fixtures/
+	testFixturePath = filepath.Join(pwd, "..", "ead", "testdata")
+
+	var eadPath = filepath.Join(testFixturePath, "fixtures", "ead-files", partner, eadid+".xml")
+	var xmlDir = filepath.Join(testFixturePath, "golden", partner, eadid)
+
+	sc := testutils.GetSolrClientMock()
+	err = sc.InitMock(xmlDir)
+	if err != nil {
+		t.Errorf("Error setting Solr client: %s", err)
+		t.FailNow()
+	}
+
+	// setup error events
+	var errorEvents []testutils.ErrorEvent
+	errorEvents = append(errorEvents, testutils.ErrorEvent{CallerName: "Delete", ErrorMessage: "error during initial Delete", CallCount: 1})
+	sc.ErrorEvents = errorEvents
 
 	// Set the Solr client
 	SetSolrClient(sc)
