10f8cb5410f1af074a6d55849d2612f72bd55bba
jgpawletko <jgpawletko@gmail.com>
2025-03-04 16:56:27 -0500

    add additional cmd/index tests

--- pkg/cmd/index/index_test.go
+++ pkg/cmd/index/index_test.go
@@ -8,6 +8,37 @@ import (
 )
 
 func TestInitLoggerError(t *testing.T) {
+	// ensure that the environment variable is set
+	os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+
+	testutils.SetCmdFlag(IndexCmd, "file", "testdata/ead.xml")
+	testutils.SetCmdFlag(IndexCmd, "logging-level", "INVALID-LOGGING-LEVEL")
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+
+	if gotStdOut == "" {
+		t.Errorf("expected data on StdOut but got nothing")
+	}
+
+	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't initialize logger: ERROR: couldn't set log level:")
+}
+
+func TestMissingFileArgument(t *testing.T) {
+	// ensure that the environment variable is set
+	os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+
+	// clear the file flag
+	testutils.SetCmdFlag(IndexCmd, "file", "")
+	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+
+	if gotStdOut == "" {
+		t.Errorf("expected data on StdOut but got nothing")
+	}
+
+	testutils.CheckStringContains(t, gotStdOut, "ERROR: EAD file path not set")
+}
+
+func TestInitSolrClientError(t *testing.T) {
 	// ensure that the environment variable is NOT set
 	os.Setenv("SOLR_ORIGIN_WITH_PORT", "")
 
@@ -20,4 +51,18 @@ func TestInitLoggerError(t *testing.T) {
 	}
 
 	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't initialize Solr client: 'SOLR_ORIGIN_WITH_PORT' environment variable not set")
+}
+
+func TestIndexingError(t *testing.T) {
+	os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+
+	testutils.SetCmdFlag(IndexCmd, "file", "testdata/ead.xml")
+	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+
+	if gotStdOut == "" {
+		t.Errorf("expected data on StdOut but got nothing")
+	}
+
+	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't index EAD file: EAD file path must be absolute: testdata/ead.xml")
 }
