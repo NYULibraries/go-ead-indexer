082dae25577a56ce29dfc3e47866dd898adae249
jgpawletko <jgpawletko@gmail.com>
2025-03-05 14:16:42 -0500

    update cmd/index/index_test.go
    Add TestLoggerLevelArgument() to confirm that logger-level argument is
      being passed to logger properly

--- pkg/cmd/index/index_test.go
+++ pkg/cmd/index/index_test.go
@@ -27,6 +27,25 @@ func TestInitLoggerError(t *testing.T) {
 	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't initialize logger: ERROR: couldn't set log level:")
 }
 
+func TestLoggerLevelArgument(t *testing.T) {
+	// ensure that the environment variable is set
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	if err != nil {
+		t.Errorf("error setting environment variable: %v", err)
+		t.FailNow()
+	}
+
+	testutils.SetCmdFlag(IndexCmd, "file", "testdata/ead.xml")
+	testutils.SetCmdFlag(IndexCmd, "logging-level", "none")
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+
+	if gotStdOut == "" {
+		t.Errorf("expected data on StdOut but got nothing")
+	}
+
+	testutils.CheckStringContains(t, gotStdOut, `Logging level set to \"none\"`)
+}
+
 func TestMissingFileArgument(t *testing.T) {
 	// ensure that the environment variable is set
 	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
