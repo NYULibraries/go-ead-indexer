565af4d170237cc0b94acb7bb86aca9c96d0ef68
jgpawletko <jgpawletko@gmail.com>
2025-04-01 09:51:16 -0400

    wrap lines in cmd/index/index_test.go

--- pkg/cmd/index/index_test.go
+++ pkg/cmd/index/index_test.go
@@ -15,7 +15,8 @@ func TestDelete_Cancel(t *testing.T) {
 	resetDeleteArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -24,20 +25,23 @@ func TestDelete_Cancel(t *testing.T) {
 	testutils.SetCmdFlag(DeleteCmd, "eadid", "fales_mss460")
 	testutils.SetCmdFlag(DeleteCmd, "assume-yes", "false")
 
-	gotStdOut, _, _ := testutils.WriteToStdinCaptureCmdStdoutStderrE(runDeleteCmd, DeleteCmd, []string{}, "n\n")
+	gotStdOut, _, _ := testutils.WriteToStdinCaptureCmdStdoutStderrE(
+		runDeleteCmd, DeleteCmd, []string{}, "n\n")
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, fmt.Sprintf("Deletion canceled for EADID: '%s'", eadID))
+	testutils.CheckStringContains(t, gotStdOut,
+		fmt.Sprintf("Deletion canceled for EADID: '%s'", eadID))
 }
 
 func TestDelete_CancelAfterBadInput(t *testing.T) {
 	resetDeleteArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -46,21 +50,24 @@ func TestDelete_CancelAfterBadInput(t *testing.T) {
 	testutils.SetCmdFlag(DeleteCmd, "eadid", "fales_mss460")
 	testutils.SetCmdFlag(DeleteCmd, "assume-yes", "false")
 
-	gotStdOut, _, _ := testutils.WriteToStdinCaptureCmdStdoutStderrE(runDeleteCmd, DeleteCmd, []string{}, "WAFFLES\nn\n")
+	gotStdOut, _, _ := testutils.WriteToStdinCaptureCmdStdoutStderrE(
+		runDeleteCmd, DeleteCmd, []string{}, "WAFFLES\nn\n")
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
 	testutils.CheckStringContains(t, gotStdOut, "Please enter 'y' or 'n':")
-	testutils.CheckStringContains(t, gotStdOut, fmt.Sprintf("Deletion canceled for EADID: '%s'", eadID))
+	testutils.CheckStringContains(t, gotStdOut,
+		fmt.Sprintf("Deletion canceled for EADID: '%s'", eadID))
 }
 
 func TestDelete_Error(t *testing.T) {
 	resetDeleteArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -70,20 +77,24 @@ func TestDelete_Error(t *testing.T) {
 	testutils.SetCmdFlag(DeleteCmd, "logging-level", "debug")
 	testutils.SetCmdFlag(DeleteCmd, "assume-yes", "true")
 
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd, DeleteCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd,
+		DeleteCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't delete data for EADID: This#Is^Not!A(Valid*EADID error:")
+	testutils.CheckStringContains(t, gotStdOut,
+		`ERROR: couldn't delete data for EADID: `+
+			`This#Is^Not!A(Valid*EADID error:`)
 }
 
 func TestDelete_InitLoggerError(t *testing.T) {
 	resetDeleteArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -93,12 +104,14 @@ func TestDelete_InitLoggerError(t *testing.T) {
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "INVALID-LOGGING-LEVEL")
 	testutils.SetCmdFlag(DeleteCmd, "assume-yes", "true")
 
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd, DeleteCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd,
+		DeleteCmd, []string{})
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't initialize logger: ERROR: unsupported logging level:")
+	testutils.CheckStringContains(t, gotStdOut,
+		"ERROR: couldn't initialize logger: ERROR: unsupported logging level:")
 }
 
 func TestDelete_InitSolrClientError(t *testing.T) {
@@ -115,19 +128,23 @@ func TestDelete_InitSolrClientError(t *testing.T) {
 	testutils.SetCmdFlag(DeleteCmd, "logging-level", "debug")
 	testutils.SetCmdFlag(DeleteCmd, "assume-yes", "true")
 
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd, DeleteCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd,
+		DeleteCmd, []string{})
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, `ERROR: couldn't initialize Solr client: error creating Solr client: parse \"this is not a valid url\": invalid URI for request`)
+	testutils.CheckStringContains(t, gotStdOut,
+		`ERROR: couldn't initialize Solr client: error creating Solr client: `+
+			`parse \"this is not a valid url\": invalid URI for request`)
 }
 
 func TestDelete_MissingEADID(t *testing.T) {
 	resetDeleteArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -136,7 +153,8 @@ func TestDelete_MissingEADID(t *testing.T) {
 	testutils.SetCmdFlag(DeleteCmd, "eadid", "")
 	testutils.SetCmdFlag(DeleteCmd, "assume-yes", "true")
 
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd, DeleteCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runDeleteCmd,
+		DeleteCmd, []string{})
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
@@ -195,7 +213,8 @@ func TestIndex_ArgumentValidation(t *testing.T) {
 		case want != "" && got == nil:
 			t.Errorf("expected an error but got nothing")
 		case (want != "" && got != nil) && (got.Error() != want):
-			t.Errorf("expected error message: '%s', but got '%s'", want, got.Error())
+			t.Errorf("expected error message: '%s', but got '%s'", want,
+				got.Error())
 		}
 	}
 }
@@ -204,7 +223,8 @@ func TestIndex_CannotDetermineIndexingCase(t *testing.T) {
 	resetIndexArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -214,20 +234,23 @@ func TestIndex_CannotDetermineIndexingCase(t *testing.T) {
 	testutils.SetCmdFlag(IndexCmd, "git-repo", "")
 	testutils.SetCmdFlag(IndexCmd, "commit", "")
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, eMsgCouldNotDetermineIndexingCase)
+	testutils.CheckStringContains(t, gotStdOut,
+		eMsgCouldNotDetermineIndexingCase)
 }
 
 func TestIndexEAD_BadFileArgument(t *testing.T) {
 	resetIndexArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -240,22 +263,26 @@ func TestIndexEAD_BadFileArgument(t *testing.T) {
 	}
 
 	// set the file flag to a non-existent file
-	testutils.SetCmdFlag(IndexCmd, "file", filepath.Join(dir, "testdata", "fixtures", "edip", "no_file_here.xml"))
+	testutils.SetCmdFlag(IndexCmd, "file",
+		filepath.Join(dir, "testdata", "fixtures", "edip", "no_file_here.xml"))
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, "ERROR: EAD file does not exist: ")
+	testutils.CheckStringContains(t, gotStdOut,
+		"ERROR: EAD file does not exist: ")
 }
 
 func TestIndexEAD_Error(t *testing.T) {
 	resetIndexArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -268,22 +295,27 @@ func TestIndexEAD_Error(t *testing.T) {
 	}
 
 	// set the file flag to an existing file that is not a valid EAD file
-	testutils.SetCmdFlag(IndexCmd, "file", filepath.Join(dir, "testdata", "fixtures", "edip", "bad_ead.xml"))
+	testutils.SetCmdFlag(IndexCmd, "file",
+		filepath.Join(dir, "testdata", "fixtures", "edip", "bad_ead.xml"))
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't index EAD file: No <ead> tag with the expected structure was found")
+	testutils.CheckStringContains(t, gotStdOut,
+		`ERROR: couldn't index EAD file: No <ead> tag `+
+			`with the expected structure was found`)
 }
 
 func TestIndexEAD_InitLoggerError(t *testing.T) {
 	resetIndexArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -291,13 +323,15 @@ func TestIndexEAD_InitLoggerError(t *testing.T) {
 
 	testutils.SetCmdFlag(IndexCmd, "file", "testdata/ead.xml")
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "INVALID-LOGGING-LEVEL")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't initialize logger: ERROR: unsupported logging level:")
+	testutils.CheckStringContains(t, gotStdOut,
+		"ERROR: couldn't initialize logger: ERROR: unsupported logging level:")
 }
 
 func TestIndexEAD_InitSolrClientError(t *testing.T) {
@@ -317,24 +351,30 @@ func TestIndexEAD_InitSolrClientError(t *testing.T) {
 	}
 
 	// set the file flag to an existing file
-	testutils.SetCmdFlag(IndexCmd, "file", filepath.Join(dir, "testdata", "fixtures", "edip", "bad_ead.xml"))
+	testutils.SetCmdFlag(IndexCmd, "file",
+		filepath.Join(dir, "testdata", "fixtures", "edip", "bad_ead.xml"))
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, `ERROR: couldn't initialize Solr client: error creating Solr client: parse \"this is not a valid url\": invalid URI for request`)
+	testutils.CheckStringContains(t, gotStdOut,
+		`ERROR: couldn't initialize Solr client: error creating Solr client:`+
+			` parse \"this is not a valid url\": invalid URI for request`)
 }
 
 func TestIndexEAD_LoggerLevelArgument(t *testing.T) {
 	resetIndexArgs()
 
-	testLogLevel := "debug" // this value MUST BE DIFFERENT from the default logging level
+	// this value MUST BE DIFFERENT from the default logging level
+	testLogLevel := "debug"
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -342,13 +382,15 @@ func TestIndexEAD_LoggerLevelArgument(t *testing.T) {
 
 	testutils.SetCmdFlag(IndexCmd, "file", "testdata/ead.xml")
 	testutils.SetCmdFlag(IndexCmd, "logging-level", testLogLevel)
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, fmt.Sprintf(`Logging level set to \"%s\"`, testLogLevel))
+	testutils.CheckStringContains(t, gotStdOut,
+		fmt.Sprintf(`Logging level set to \"%s\"`, testLogLevel))
 }
 
 func TestIndexEAD_MissingSolrOriginEnvVariableError(t *testing.T) {
@@ -368,22 +410,27 @@ func TestIndexEAD_MissingSolrOriginEnvVariableError(t *testing.T) {
 	}
 
 	// set the file flag to an existing file
-	testutils.SetCmdFlag(IndexCmd, "file", filepath.Join(dir, "testdata", "fixtures", "edip", "bad_ead.xml"))
+	testutils.SetCmdFlag(IndexCmd, "file",
+		filepath.Join(dir, "testdata", "fixtures", "edip", "bad_ead.xml"))
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, "ERROR: couldn't initialize Solr client: 'SOLR_ORIGIN_WITH_PORT' environment variable not set")
+	testutils.CheckStringContains(t, gotStdOut,
+		`ERROR: couldn't initialize Solr client: `+
+			`'SOLR_ORIGIN_WITH_PORT' environment variable not set`)
 }
 
 func TestIndexEAD_UnsetLoggingLevelArgument(t *testing.T) {
 	resetIndexArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -391,20 +438,23 @@ func TestIndexEAD_UnsetLoggingLevelArgument(t *testing.T) {
 
 	testutils.SetCmdFlag(IndexCmd, "file", "testdata/ead.xml")
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
 	}
 
-	testutils.CheckStringContains(t, gotStdOut, `Logging level set to \"info\"`)
+	testutils.CheckStringContains(t, gotStdOut,
+		`Logging level set to \"info\"`)
 }
 
 func TestIndexGitCommit_BadGitRepoArgument(t *testing.T) {
 	resetIndexArgs()
 
 	// ensure that the environment variable is set
-	err := os.Setenv("SOLR_ORIGIN_WITH_PORT", "http://www.example.com:8983/solr")
+	err := os.Setenv("SOLR_ORIGIN_WITH_PORT",
+		"http://www.example.com:8983/solr")
 	if err != nil {
 		t.Errorf("error setting environment variable: %v", err)
 		t.FailNow()
@@ -418,10 +468,13 @@ func TestIndexGitCommit_BadGitRepoArgument(t *testing.T) {
 
 	// set the file flag to a non-existent file
 	//testutils.SetCmdFlag(IndexCmd, "file", "")
-	testutils.SetCmdFlag(IndexCmd, "git-repo", filepath.Join(dir, "testdata", "fixtures", "this-repo-does-not-exist"))
-	testutils.SetCmdFlag(IndexCmd, "commit", "a5ca6cca30fc08cfc13e4f1492dbfbbf3ec7cf63")
+	testutils.SetCmdFlag(IndexCmd, "git-repo",
+		filepath.Join(dir, "testdata", "fixtures", "this-repo-does-not-exist"))
+	testutils.SetCmdFlag(IndexCmd, "commit",
+		"a5ca6cca30fc08cfc13e4f1492dbfbbf3ec7cf63")
 	testutils.SetCmdFlag(IndexCmd, "logging-level", "debug")
-	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd, IndexCmd, []string{})
+	gotStdOut, _, _ := testutils.CaptureCmdStdoutStderrE(runIndexCmd,
+		IndexCmd, []string{})
 
 	if gotStdOut == "" {
 		t.Errorf("expected data on StdOut but got nothing")
@@ -431,8 +484,8 @@ func TestIndexGitCommit_BadGitRepoArgument(t *testing.T) {
 }
 
 func TestLocalLogLevels(t *testing.T) {
-	// this is a regression test to ensure that the local log levels are still valid
-	// if this test fails, the local log levels need to be updated
+	// this is a regression test to ensure that the local log levels are still
+	// valid if this test fails, the local log levels need to be updated
 	loggerAvailableLevels := log.GetValidLevelOptionStrings()
 	for _, level := range localLogLevels {
 		if !slices.Contains(loggerAvailableLevels, level) {
